b8fa19cbb2365bc5c43e237bd6119b8266bb951e
Kit <dev_kit@icloud.com>
2015-06-23 18:09:52 -0600

    initial commit

---
+++ spec/rails_helper.rb
@@ -0,0 +1,15 @@
+ENV['RAILS_ENV'] ||= 'test'
+require 'spec_helper'
+require File.expand_path('../../config/environment', __FILE__)
+require 'rspec/rails'
+require 'capybara/rails'
+require_relative "support/database_cleaner"
+
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.include FactoryGirl::Syntax::Methods
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+end
